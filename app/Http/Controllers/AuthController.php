4e8b5aa41a485f1a4ad4ad5bcb8fcdca0c70e0a9
Karlos Raya <karlos.raya@gmail.com>
2020-01-04 01:50:29 +0800

    Initial commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         $user = User::create($input); 
 
         return response()->json([
-            'message' => 'Successfully created user!'
+            'message' => 'User successfully created!'
         ], 201);
     }
   
@@ -38,36 +38,42 @@ class AuthController extends Controller
             'password' => 'required|string',
             'remember_me' => 'boolean'
         ]);
+
         $credentials = request(['username', 'password']);
+
         if(!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
+            return response()->json(['error'=>'Login failed! Please double check username and passowrd and try again.'], 401);
+        
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
-        if ($request->remember_me)
+
+        if ($request->remember_me) {
             $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+
         $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
+        
+        $success['token'] =  $tokenResult->accessToken; 
+
+        return response()->json(['success' => $success]); 
     }
   
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
+        return response()->json(['success'=>true]); ; 
     }
   
     public function user(Request $request)
     {
         return response()->json($request->user());
     }
+
+    public function getUsers() 
+    { 
+        $users = DB::table('users')->get();
+
+        return response()->json($users, $this-> successStatus); 
+    } 
 }
--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,13 @@ Route::group([
     'prefix' => 'auth'
 ], function () {
     Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
+    Route::post('register', 'AuthController@signup');
   
     Route::group([
       'middleware' => 'auth:api'
     ], function() {
         Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
+        Route::get('', 'AuthController@user');
     });
 });
+
