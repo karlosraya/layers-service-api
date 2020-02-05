691c13d07b74a5826f51fa107b19b630e3689a66
Karlos Raya <karlos.raya@gmail.com>
2020-02-06 02:21:13 +0800

    user role related files

---
+++ database/migrations/2020_01_28_183708_create_user_roles_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('userId');
+            $table->enum('role', ['administrator', 
+                                  'manageUsers', 
+                                  'lockData', 
+                                  'viewFeedsDelivery', 
+                                  'editFeedsDelivery', 
+                                  'viewEggProduction', 
+                                  'editEggProduction', 
+                                  'deleteEggProduction', 
+                                  'viewGradedEggs', 
+                                  'editGradedEggs',
+                                  'viewInvoice',
+                                  'editInvoice',
+                                  'deleteInvoice',
+                                  'viewHouse',
+                                  'editHouse',
+                                  'viewBatch',
+                                  'editBatch',
+                                  'viewCustomer',
+                                  'editCustomer',
+                                  'viewPrice',
+                                  'editPrice']);
+            $table->string('houseName')->nullable();
+            $table->string('lastInsertUpdateBy');
+            $table->dateTime('lastInsertUpdateTS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
